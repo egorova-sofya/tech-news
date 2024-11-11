da71c53fe90b4af2e6a74c8d2982f9554cce7bf8
egorova-sofya <EgorovaSjob@yandex.ru>
2024-11-11 14:29:44 +0300

    added eslint-plugin-i18next

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,7 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
+import i18next from "eslint-plugin-i18next";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
@@ -10,7 +11,11 @@ export default [
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
-
+  // i18next.configs.flat.recommended,
+  // i18next.configs["flat/recommended"],
+  {
+    extends: ["plugin:i18next/recommended"],
+  },
   {
     settings: {
       react: {
@@ -23,6 +28,10 @@ export default [
     rules: {
       "react/react-in-jsx-scope": "off",
       "@typescript-eslint/no-unused-vars": "warn",
+      "i18next/no-literal-string": [
+        "error",
+        { markupOnly: true, ignoreAttribute: ["data-testid", "to"] },
+      ],
     },
   },
 ];
--- src/pages/ProfilePage/ui/ProfilePage.tsx
+++ src/pages/ProfilePage/ui/ProfilePage.tsx
@@ -2,7 +2,7 @@ import { useTranslation } from "react-i18next";
 
 const ProfilePage = () => {
   const { t } = useTranslation("profile");
-  return <div>{t("Страница профиля")}weedfsfd</div>;
+  return <div>{t("Страница профиля")}</div>;
 };
 
 export default ProfilePage;
