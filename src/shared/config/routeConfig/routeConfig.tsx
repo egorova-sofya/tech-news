5e7374448d2c2cc507a9383a1959c8fdfe5d2503
egorova-sofya <EgorovaSjob@yandex.ru>
2024-11-15 20:39:28 +0300

    added not found page and loader

--- src/shared/config/routeConfig/routeConfig.tsx
+++ src/shared/config/routeConfig/routeConfig.tsx
@@ -1,15 +1,18 @@
 import { MainPage } from "pages/MainPage";
+import { NotFoundPage } from "pages/NotFoundPage";
 import { ProfilePage } from "pages/ProfilePage";
 import { RouteProps } from "react-router-dom";
 
 export enum AppRoutes {
   MAIN = "main",
   PROFILE = "profile",
+  NOT_FOUND = "not_found",
 }
 
 export const RouterPath: Record<AppRoutes, string> = {
   [AppRoutes.MAIN]: "/",
   [AppRoutes.PROFILE]: "/profile",
+  [AppRoutes.NOT_FOUND]: "*",
 };
 
 export const routeConfig: Record<AppRoutes, RouteProps> = {
@@ -20,5 +23,9 @@ export const routeConfig: Record<AppRoutes, RouteProps> = {
   [AppRoutes.PROFILE]: {
     path: RouterPath[AppRoutes.PROFILE],
     element: <ProfilePage />,
+  },
+  [AppRoutes.NOT_FOUND]: {
+    path: RouterPath[AppRoutes.NOT_FOUND],
+    element: <NotFoundPage />,
   },
 };
